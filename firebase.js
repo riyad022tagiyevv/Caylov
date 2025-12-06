bf86c666baa56b070d50102a8fe53e44a4d16883
riyad022tagiyevv <97640853+riyad022tagiyevv@users.noreply.github.com>
2025-12-07 01:53:40 +0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Firebase JS SDK v7.20.0 və daha sonra measurementId optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCwnn6JY3f-Nv9NWx2beIUjAnyVCqU749E",
+  authDomain: "cay-love-you.firebaseapp.com",
+  databaseURL: "https://cay-love-you-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "cay-love-you",
+  storageBucket: "cay-love-you.firebasestorage.app",
+  messagingSenderId: "526657108236",
+  appId: "1:526657108236:web:8ce574d880d232c754b129",
+  measurementId: "G-XVP460DCK9"
+};
+
+// Firebase initialize
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
